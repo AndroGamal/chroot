b76d4992f439ebb41d931d1c8f3d81686acb591f
Andro <andogamal21@gmail.com>
2020-05-13 21:31:50 +0300

    chroot

--- app/src/main/java/com/example/chroot/run.java
+++ app/src/main/java/com/example/chroot/run.java
@@ -7,13 +7,21 @@ import android.content.Intent;
 import java.io.IOException;
 
 public class run extends BroadcastReceiver {
+   ;
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
-            Process p=Runtime.getRuntime().exec("bootkali");
-            p.getOutputStream().write("mount -t ramfs  ramfs /dev\n".getBytes());
-            p.getOutputStream().write("mount proc /proc -t proc\n".getBytes());
-            p.getOutputStream().write("mount  -t sysfs sysfs /sys\n".getBytes());
+            Process kali=Runtime.getRuntime().exec("su");
+            kali.getOutputStream().write("bootkali\n".getBytes());
+            kali.getOutputStream().write("mount -t ramfs  ramfs /dev\n".getBytes());
+            kali.getOutputStream().write("mount proc /proc -t proc\n".getBytes());
+            kali.getOutputStream().write("mount  -t sysfs sysfs /sys\n".getBytes());
+            kali.getOutputStream().write("exit\n".getBytes());
+            kali.getOutputStream().write("mount -o bind /dev  data/local/kali-armhf/dev\n".getBytes());
+            kali.getOutputStream().write ("bootkali\n".getBytes());
+            kali.getOutputStream().write("dd if=/dev/zero of=sdcard/swapkali bs=1024 count=75000\n".getBytes());
+            kali.getOutputStream().write("mkswap sdcard/swapkali\n".getBytes());
+            kali.getOutputStream().write("swapon -p 8 sdcard/swapkali\n".getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
