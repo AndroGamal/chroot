f15b6a9adf53efc3f2b6b75073ce7932ac8d8591
Andro <andogamal21@gmail.com>
2020-05-21 17:09:22 +0300

    chroot

--- app/src/main/java/com/example/chroot/run.java
+++ app/src/main/java/com/example/chroot/run.java
@@ -3,26 +3,33 @@ package com.example.chroot;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import java.io.IOException;
 
+
 public class run extends BroadcastReceiver {
-   ;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
-            Process kali=Runtime.getRuntime().exec("su");
-            kali.getOutputStream().write("bootkali\n".getBytes());
-            kali.getOutputStream().write("mount -t ramfs  ramfs /dev\n".getBytes());
+            Process kali = Runtime.getRuntime().exec("bootkali");
+            kali.getOutputStream().write("mount devpts /dev/pts -t devpts\n".getBytes());
+            kali.getOutputStream().write("mount -t tmpfs  tmpfs /dev\n".getBytes());
             kali.getOutputStream().write("mount proc /proc -t proc\n".getBytes());
             kali.getOutputStream().write("mount  -t sysfs sysfs /sys\n".getBytes());
-            kali.getOutputStream().write("exit\n".getBytes());
-            kali.getOutputStream().write("mount -o bind /dev  data/local/kali-armhf/dev\n".getBytes());
-            kali.getOutputStream().write("mount -o move /storage/extSdCard /data/local/kali-armhf/sdcard\n".getBytes());
-            kali.getOutputStream().write ("bootkali\n".getBytes());
-            kali.getOutputStream().write("dd if=/dev/zero of=sdcard/swapkali bs=1024 count=524000\n".getBytes());
-            kali.getOutputStream().write("mkswap sdcard/swapkali\n".getBytes());
-            kali.getOutputStream().write("swapon -p 8 sdcard/swapkali\n".getBytes());
+            kali.getOutputStream().write("mount -o remount -t rootfs  /root rootfs\n".getBytes());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Process root = Runtime.getRuntime().exec("su");
+                    root.getOutputStream().write("mount -o move /storage/extSdCard /data/local/kali-armhf/sdcard".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         } catch (IOException e) {
             e.printStackTrace();
         }
